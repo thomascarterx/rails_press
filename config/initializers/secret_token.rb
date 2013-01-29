83cbdcad9742e2495ad387904953a65356c3ef10
Eric MacAdie <Eric@MacAdie.net>
2013-01-29 13:57:25 -0600

    First commit: :

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPress::Application.config.secret_token = 'cd62da881206c6f9d6133409391beb0e452c085cf019beaf3abfe9c7c7c94b821597b012d09f634d21fdc2363b7837f20e127e7988440b390094537b6ede0f6f'
